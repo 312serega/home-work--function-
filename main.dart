7d176d3008cb55d4b78dbcc749a206dbcf481861
312serega <sergey.bmx@gmail.com>
2021-12-20 16:02:23 +0600

    function

---
+++ main.dart
@@ -0,0 +1,58 @@
+void main() {
+  List<int> intArr = [1,2,3,4,5,6,7,8];
+  var result;
+  for (var i = 0; i < intArr.length; i++) {
+      if (intArr[i] == 5) {
+        result = 'ДА!';
+        break;
+      }
+      else{
+        result = 'Нет';
+      }
+  }
+  print(result);
+
+  print(compareInt (5, 5));
+
+  print(negation (-1));
+}
+
+String compareInt (a, b){
+  if (a == b) {
+    return 'true';
+  }else{
+    return 'false';
+  }
+}
+
+negation (c){
+  if (c > 0) {
+    return 'false';
+  }else{
+    return 'true';
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// String printName( String name, int age) {
+//   var info = 'name - $name, age $age';
+//   return info;
+// }
+
+// String printName2( String name, int age) => 'name - $name, age $age';
+
+
+// int printType(){
+//   return 50;
+// }
